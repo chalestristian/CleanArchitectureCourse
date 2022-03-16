12bffa6bb4a50d9228a3273fefaf351bc719d14a
Thales Cristian <thales.cristianeugenio@gmail.com>
2022-03-16 13:21:10 -0300

    Create Repository Product

---
+++ CleanArchMvc/CleanArch.Mvc.Infra.Data/Repositories/ProductyRepository.cs
@@ -0,0 +1,57 @@
+﻿using CleanArch.Mvc.Infra.Data.Context;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Mvc.Infra.Data.Repositories
+{
+    public class ProductyRepository : IProductRepository
+    {
+
+        ApplicationDbContext _productContext;
+        public ProductyRepository(ApplicationDbContext context)
+        {
+
+        _productContext = context;
+
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContext.Add(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int? id)
+        {
+            return await _productContext.Products.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductAsync()
+        {
+            return await _productContext.Products.ToListAsync();
+        }
+
+        //eager load
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            return await _productContext.Products.Include(c => c.Category)
+                .SingleOrDefaultAsync(prop => prop.Id == id);
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productContext.Remove(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+
+        public async  Task<Product> UpdateAsync(Product product)
+        {
+            _productContext.Update(product);
+            await _productContext.SaveChangesAsync();
+            return product;
+        }
+    }
+}
