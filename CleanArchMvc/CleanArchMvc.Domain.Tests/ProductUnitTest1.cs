3611673bcf727833a2e6a9a41056108c7edc5406
Thales Cristian <thales.cristianeugenio@gmail.com>
2022-03-16 11:59:33 -0300

    Create Domain Tests - With XUnit

---
+++ CleanArchMvc/CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,73 @@
+using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,99, "product image");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id Value");
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Th", "Product Description", 9.99m, 99, "product image");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name. Too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageNameValue_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Thales", "Product Description", 9.99m, 99, "d1sas6d6as1818as9c1dsf18asd4891swadegas89d4s65d4fg89wd4as64f89w4ef8a4s89df4sd6f19AS9sdtg894qw81df89sd4gt8qwr9489adsf6sd4fg89aw4d89a4ft98eqwf489qw4ef4qdf8a4s89f4ads89f89er4g98aq4sdf894sd89gt4w89ad489sd4g89S498S4D89F49S8G4B9S8R4DDASFG89SDG9S8DG489SD4GSDD");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalide image name. Too long, maximum 250 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyImageNameValue_NoDomainException()
+        {
+            Action action = () => new Product(1, "Thales", "Product Description", 9.99m, 99, "");
+            action.Should().NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImageNameValue_NoDomainException()
+        {
+            Action action = () => new Product(1, "Thales", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_DomainExceptionNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Thales", "Product Description", 9.99m, value, null);
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid stock value.");
+        }
+
+        [Fact]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Thales", "Product Description", -9.99m, 99, "54zsdasd");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid price value.");
+        }
+    }
+}
